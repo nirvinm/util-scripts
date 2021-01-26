731aff512edeadc34282b20a170fb15b9def32e6
Nirvin M <nirvin@outlook.com>
2021-01-26 14:36:49 +0530

    Include IPv6 range in the final list

--- generate_wireguard_allowed_ip.py
+++ generate_wireguard_allowed_ip.py
@@ -3,7 +3,11 @@ import sys
 
 excl=sys.argv[1]
 
-n1 = ipaddress.ip_network('0.0.0.0/0')
-n2 = ipaddress.ip_network(excl)
-res = list(n1.address_exclude(n2))
-print(",".join([str(ip) for ip in res]))
+all_ipv6 = ipaddress.ip_network("::/0")
+all_ipv4 = ipaddress.ip_network('0.0.0.0/0')
+excluded_ipv4 = ipaddress.ip_network(excl)
+result = list(all_ipv4.address_exclude(excluded_ipv4))
+result.insert(0, all_ipv6)
+print(",".join([str(ip) for ip in result]))
+
+
